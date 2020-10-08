be32c6653b9e921af4f44cf69170ce8fb58e966e
unknown <zb.tyoma@gmail.com>
2020-10-08 11:24:44 +0700

    create project

---
+++ src/store/nodules/user.js
@@ -0,0 +1,42 @@
+// import {http} from '../../scripts/http';
+
+const state = {
+    _id: null,
+    login: null,
+    uCode: null,
+};
+
+const getters = {
+    _id: state => {
+        return state.playlist;
+    },
+    login: state => {
+        return state.play;
+    },
+    uCode: state => {
+        return state.hash;
+    }
+};
+  
+const mutations = {
+    set_id: (state, payload) => {
+        state.playlist = payload;
+    },
+    setLogin: (state, payload) => {
+        state.play = payload;
+    },
+    setUCode: (state, payload) => {
+        state.hash = payload;
+    }
+};
+
+const actions = {
+
+}
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions,
+};
